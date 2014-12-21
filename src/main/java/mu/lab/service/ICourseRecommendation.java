3c1e7496195cc40c7630afca208a4fd9cd8fa94c
cgcgbcbc <cgcgbcbc@163.com>
2014-12-21 15:06:15 +0800

    add javadoc for ICourseCommendation interface

--- src/main/java/mu/lab/service/ICourseRecommendation.java
+++ src/main/java/mu/lab/service/ICourseRecommendation.java
@@ -7,8 +7,33 @@ import mu.lab.model.Score;
  * Created by guangchen on 12/18/14 22:01.
  */
 public interface ICourseRecommendation {
+    /**
+     * Directly get 5 courses recommended for the given student.
+     * @param studentId The graphId of the student node
+     * @return Recommend course list
+     */
     public Iterable<Course> getRecommendCourse(Long studentId);
+
+    /**
+     * get n courses recommended for the given student.
+     * @param studentId The graphId of the student node
+     * @param n n
+     * @return Recommend course list
+     */
     public Iterable<Course> getRecommendCourse(Long studentId, int n);
+
+    /**
+     * NOT IMPLEMENTED YET!!!
+     * @param studentId The graphId of the student node
+     * @param courseId The graphId of a course node
+     * @return Recommend course list
+     */
     public Iterable<Course> getRecommendCourse(Long studentId, Long courseId);
+
+    /**
+     *
+     * @param scores Score list
+     * @return recommend courses based on the given scores.
+     */
     public Iterable<Course> getRecommendCourseBasedOnMockScores(Score... scores);
 }
